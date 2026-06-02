7f62ca8d97fd17d9a6c6c0ad3eab82ae2d4c16f2
lancer43 <klimenty23v@gmail.com>
2026-06-02 03:35:54 +0300

    feat(autotest): creating different file filling modes of random length for objective testing of the program

--- autotest.c
+++ autotest.c
@@ -4,10 +4,12 @@
 #include <assert.h>
 #include <string.h>
 
-#define MAX_TEST_FILENAME_LEN	8 // "test" + "<number 0-999>" + '\0'
+#define MAX_TEST_FILENAME_LEN	16 // "test" + "<number 0-999>" + '\0' С ЗАПАСОМ
 #define ACCEPTABLE_EXT_CHARS	"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_" // без точки!!!!
 
 #define NUMBER_OF_TEST_FILES	100
+#define MAX_TEST_FILE_SIZE		(5 * 1024 * 1024)
+#define NUMBER_OF_TYPES			7 // количество типов для теста из TypeFileContent_e
 
 typedef enum {
 	CONTENT_EMPTY, // пустой файл
@@ -17,7 +19,18 @@ typedef enum {
 	CONTENT_RANDOM, // файл с равномерным распределением символов (длина случайна)
 	CONTENT_UNEVEN, // файл с неравномерным распределением символов
 	CONTENT_ASCII // файл с гарантией использования каждого символа ASCII хотя бы 1 раз (распределение равномерное)
-} FileContent_e;
+} TypeFileContent_e;
+
+// количество файлов каждого типа в каждой сотне тестовых
+typedef enum {
+	WEIGHT_EMPTY = 5,
+	WEIGHT_ONE_BYTE = 5,
+	WEIGHT_MONO_SYM = 10,
+	WEIGHT_TWO_SYMS = 10,
+	WEIGHT_RANDOM = 30,
+	WEIGHT_UNEVEN = 30,
+	WEIGHT_ASCII = 10
+} TypeWeight_e;
 
 /*
 	========== ФУНКЦИОНАЛ МОДУЛЯ ==========
@@ -26,8 +39,6 @@ typedef enum {
 	- сравнение сжатых и распакованных файлов на содержание и формат
 	- замер времени сжатия и распаковки каждого файла и сведение этих данных в .csv файл (+ можно записывать формат файла, массив частот, таблицу кодов, длину файла)
 
-	========== ГЕНЕРАЦИЯ ФАЙЛОВ ==========
-	-
 
 */
 
@@ -45,9 +56,11 @@ static void create_test_extension(char extension[MAX_EXT_LENGTH]) {
 	size_t count = 1; 
 	while (count < ext_len - 1) { // оставляем последний байт под '\0'
 		char symbol = rand() % ASCII_ALP_SIZE;
-		while (!strchr(ACCEPTABLE_EXT_CHARS, symbol)) {
+		
+		while (!strchr(ACCEPTABLE_EXT_CHARS, symbol) || symbol == '\0') {
 			symbol = rand() % ASCII_ALP_SIZE;
 		}
+
 		extension[count++] = symbol;
 	}
 
@@ -62,10 +75,7 @@ static void create_test_extension(char extension[MAX_EXT_LENGTH]) {
 static void create_test_filename(char filename[MAX_TEST_FILENAME_LEN], const size_t number) {
 	assert(number < 1000); // я планирую подавать на тест 100 файлов
 	
-	char string_number[4]; // 0-999
-	
-	snprintf(string_number, sizeof(string_number), "%zu", number);
-	snprintf(filename, MAX_TEST_FILENAME_LEN, "%s%s", filename, string_number);
+	snprintf(filename, MAX_TEST_FILENAME_LEN, "test%zu", number);
 }
 
 /*
@@ -85,23 +95,251 @@ static void create_test_path(
 }
 
 /*
+	@brief Случайный размер тестового файла
+	@return Возвращает размер в байтах
+*/
+static size_t create_test_random_file_size(void) {
+	size_t big_random = 0;
+
+	#if RAND_MAX < 65535
+		// если мы на Windows (MSVC), где RAND_MAX всего 32767
+		big_random = ((size_t)rand() << 15) | (size_t)rand();
+	#else
+		// если мы на Linux/macOS, где RAND_MAX это 2147483647
+		big_random = (size_t)rand();
+	#endif
+
+		return big_random % MAX_TEST_FILE_SIZE;
+}
+
+/*
+	@brief Функция общего алгоритма заполнения файла (содержимое уже сформировано)
+	@param stream - поток для записи
+	@param file_len - длина файла в байтах
+	@param buf - стековый буфер для заполнения
+	@return успех - 1, иначе - 0
+*/
+static int fill_file(FILE* stream, const size_t file_len, const char buf[WRITE_BUFFER_SIZE]) {
+	size_t remaining = file_len;
+
+	while (remaining > 0) {
+		// выбираем порцию для записи: либо целый буфер, либо остаток
+		size_t portion = (remaining > WRITE_BUFFER_SIZE) ? WRITE_BUFFER_SIZE : remaining;
+
+		if (fwrite(buf, sizeof(char), portion, stream) < portion) {
+
+			return 0;
+		}
+
+		remaining -= portion;
+	}
+
+	return 1;
+}
+
+/*
+	@brief Генерация содержания файла из 1 символа заданной длины
+	@param stream - поток для записи
+	@param file_len - длина файла
+	@return успех - 1, иначе 0
+*/
+static int generate_mono_sym(FILE* stream, const size_t file_len) {
+	char buf[WRITE_BUFFER_SIZE]; // стековый буфер
+	char rand_sym = (char)(rand() % ASCII_ALP_SIZE); // символ для заполнения
+
+	// заполняем одинаковыми символами
+	memset(buf, rand_sym, WRITE_BUFFER_SIZE);
+
+	if (!fill_file(stream, file_len, buf)) {
+		perror("[MONO_SYM] Ошибка заполнения тестового файла");
+		return 0;
+	}
+
+	return 1;
+}
+
+/*
+	@brief Генерация содержания файла из 2 символов заданной длины
+	@param stream - поток для записи
+	@param file_len - длина файла
+	@return успех - 1, иначе 0
+*/
+static int generate_two_syms(FILE* stream, const size_t file_len) {
+	char buf[WRITE_BUFFER_SIZE]; // стековый буфер
+	char rand_sym1 = rand() % ASCII_ALP_SIZE; // 1 символ для заполнения
+	char rand_sym2 = rand() % ASCII_ALP_SIZE; // 2 символ для заполнения
+
+	// --- формируем рандомную последовательность из двух символов на весь стековый буфер ---
+	// мы будем копировать её пока не забъем файл нужным количеством байт
+	// объективность теста сохраняется, т.к. алгоритм Хаффмана читает по 1 байту и ему 
+	// плевать на закономерно повторяющуюся последовательность
+	// мы точно так же "рандомно" формируем распределение символов в рамках стекового буфера
+	for (size_t i = 0; i < WRITE_BUFFER_SIZE; i++) {
+		
+		// [TODO] чтобы не вызывать 4096 раз функцию rand() можно упаковывать рандом пачками по 32 или 64 бита через >> <<
+		buf[i] = (rand() % 2) ? rand_sym1 : rand_sym2;
+	}
+
+	if (!fill_file(stream, file_len, buf)) {
+		perror("[TWO_SYMS] Ошибка заполнения тестового файла");
+		return 0;
+	}
+
+	return 1;
+}
+
+/*
+	@brief Генерация рандомного содержания файла заданной длины 
+	@param stream - поток для записи
+	@param file_len - длина файла
+	@return успех - 1, иначе 0
+*/
+static int generate_random_content(FILE* stream, const size_t file_len) {
+	char buf[WRITE_BUFFER_SIZE]; // стековый буфер
+
+	for (size_t i = 0; i < WRITE_BUFFER_SIZE; i++) {
+		// [TODO] чтобы не вызывать 4096 раз функцию rand() можно упаковывать рандом пачками по 32 или 64 бита через >> <<
+		buf[i] = rand() % ASCII_ALP_SIZE;
+	}
+
+	if (!fill_file(stream, file_len, buf)) {
+		perror("[RANDOM] Ошибка заполнения тестового файла");
+		return 0;
+	}
+
+	return 1;
+}
+
+/*
+	@brief Генерация неравномерно распределённого содержания файла заданной длины
+	@param stream - поток для записи
+	@param file_len - длина файла
+	@return успех - 1, иначе 0
+*/
+static int generate_uneven_content(FILE* stream, const size_t file_len) {
+	char buf[WRITE_BUFFER_SIZE]; // стековый буфер
+
+	size_t n = (rand() % 9) + 2; // шаг с которым будем вставлять "особый" символ, который чаще всего повторяется
+	char special_sym = (char)(rand() % ASCII_ALP_SIZE); // особый символ
+
+	for (size_t i = 0; i < WRITE_BUFFER_SIZE; i++) {
+		if (i % n == 0) {
+			buf[i] = special_sym;
+		}
+		else {
+			// [TODO] чтобы не вызывать 4096 раз функцию rand() можно упаковывать рандом пачками по 32 или 64 бита через >> <<
+			buf[i] = (char)(rand() % ASCII_ALP_SIZE);
+		}
+	}
+
+	if (!fill_file(stream, file_len, buf)) {
+		perror("[UNEVEN] Ошибка заполнения тестового файла");
+		return 0;
+	}
+
+	return 1;
+}
+
+/*
+	@brief Генерация содержания файла с гарантией всех 256 символов ASCII и равномерным распределением заданной длины
+	@param stream - поток для записи
+	@param file_len - длина файла
+	@return успех - 1, иначе 0
+*/
+static int generate_ascii_content(FILE* stream, const size_t file_len) {
+	char buf[ASCII_ALP_SIZE]; // стековый буфер
+
+	// заполняем файл
+	for (size_t i = 0; i < ASCII_ALP_SIZE; i++) {
+		buf[i] = (char)i;
+	}
+
+	// на случай если длина файла оказалась меньше 256 байт
+	if (file_len <= ASCII_ALP_SIZE) {
+		if (fwrite(buf, sizeof(*buf), file_len, stream) < file_len) {
+			perror("[ASCII] Ошибка заполнения тестового файла");
+			return 0;
+		}
+
+		return 1;
+	} 
+
+	if (fwrite(buf, sizeof(*buf), ASCII_ALP_SIZE, stream) < ASCII_ALP_SIZE) {
+		perror("[ASCII] Ошибка заполнения тестового файла");
+		return 0;
+	}
+
+	if (!generate_random_content(stream, file_len - ASCII_ALP_SIZE)) return 0;
+
+	return 1;
+}
+
+/*
 	@brief Создание случайного содержания для тестового файла
 	@param stream - поток для записи
+	@param type - тип содержания
+	@return успех - 1, иначе - 0
 */
-void create_test_random_content(FILE* stream) {
-	
+static int create_test_random_content(FILE* stream, size_t file_len, TypeFileContent_e type) {
+	switch (type) {
+		case CONTENT_EMPTY: {
+			return 1;
+		}
+		case CONTENT_ONE_BYTE: {
+			// забить 1 битом
+			char rand_sym = rand() % ASCII_ALP_SIZE;
+			if (fwrite(&rand_sym, sizeof(rand_sym), 1, stream) < 1) return 0;
+
+			return 1;
+		}
+		case CONTENT_MONO_SYM: {
+			// рандом длина из 1 символа
+			if (!generate_mono_sym(stream, file_len)) return 0;
+
+			return 1;
+		}
+		case CONTENT_TWO_SYMS: {
+			// рандом длина из двух символов
+			if (!generate_two_syms(stream, file_len)) return 0;
+
+			return 1;
+		}
+		case CONTENT_RANDOM: {
+			// равномерное распределение
+			if (!generate_random_content(stream, file_len)) return 0;
+
+			return 1;
+		}
+		case CONTENT_UNEVEN: {
+			// неравномерное распределение
+			if (!generate_uneven_content(stream, file_len)) return 0;
+
+			return 1;
+		}
+		case CONTENT_ASCII: {
+			// гарантия 256 символов + равномерное распределение
+			if (!generate_ascii_content(stream, file_len)) return 0;
+
+			return 1;
+		}
+		default:
+			return 0;
+	}
+
+	return 1; // на всякий случай
 }
 
 /*
 	@brief Создание тестового файла с рандомным содержанием
 	@param number - порядковый номер файла
+	@param type - тип содержания файл
+	@param path - путь (передается наружу для подведения статистики)
+	@return успех - 1, иначе - 0
 */
-static int create_test_random_file(size_t number) {
+static int create_test_random_file(size_t number, size_t file_len, TypeFileContent_e type, char test_path[MAX_PATH_LEN]) {
 	char* test_dir = "./autotest_files/"; // папка для тестовых файлов
 	char test_filename[MAX_TEST_FILENAME_LEN] = "test";
 	char test_extension[MAX_EXT_LENGTH];
-	
-	char test_path[MAX_PATH_LEN];
 
 	create_test_filename(test_filename, number);
 	create_test_extension(test_extension);
@@ -110,22 +348,88 @@ static int create_test_random_file(size_t number) {
 	printf("\n[DEBUG] сформированное расширение: %s", test_extension);
 	printf("\n[DEBUG] сформированный путь: %s", test_path);
 
-	FILE* new_file = fopen(test_path, "w");
+	FILE* new_file = fopen(test_path, "wb");
 	if (!new_file) {
 		printf("\nОшибка: неудачное создание файла");
 		return 0;
 	}
 
 	// функция генерации рандомного содержания
+	create_test_random_content(new_file, file_len, type);
 
 	fclose(new_file);
 	return 1;
 }
 
+
+/*
+	======= ПЛАН =======
+	запускаем автотест
+
+	цикл (NUMBER_OF_TEST_FILES раз) {
+		- создать файл
+		- забить его содержанием (по выбору типа контента) рандомной длины до 50Мб
+		- сжать файл
+		- распаковать файл
+		- проверить файлы на одинаковость
+
+		- занести данные для аналитики в .csv
+
+		- удалить файлы с диска
+	}
+
+
+*/
+
+/*
+		
+*/
+
 int run_autotest_files(void) {
-	for (size_t i = 0; i < NUMBER_OF_TEST_FILES; i++) {
-		if (!create_test_random_file(i)) {
-			printf("\nОшибка: неудачное создание файла");
+	size_t weight = 0; 
+	size_t idx = 0;
+
+	TypeWeight_e weights[] = {
+		WEIGHT_EMPTY,
+		WEIGHT_ONE_BYTE,
+		WEIGHT_MONO_SYM,
+		WEIGHT_TWO_SYMS,
+		WEIGHT_RANDOM,
+		WEIGHT_UNEVEN,
+		WEIGHT_ASCII,
+	};
+
+	for (TypeFileContent_e type = 0; type < NUMBER_OF_TYPES; type++) {
+		weight += (size_t)weights[type];
+		
+		while (idx < weight) {
+			size_t file_len = 0;
+
+			if (type == CONTENT_EMPTY) {
+				file_len = 0;
+			}
+			else if (type == CONTENT_ONE_BYTE) {
+				file_len = 1;
+			}
+			else {
+				file_len = create_test_random_file_size();
+			}
+
+			char path[MAX_PATH_LEN] = { 0 };
+
+			if (!create_test_random_file(idx, file_len, type, path)) return 0;
+
+			/*
+				сжать
+				распаковать
+				сравнить
+				занести данные в .csv
+				удалить
+			*/
+
+			idx++;
 		}
 	}
+
+	return 1;
 }
